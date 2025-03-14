b2f42f06734a379a30a9452914e7569a1c801773
unknown <vinicius_24019@ETEC.FURLAN>
2025-03-14 10:54:28 -0300

    segundo commit

---
+++ app/_layout.js
@@ -0,0 +1,50 @@
+import { View, Text} from 'react-native';
+
+import React, { useEffect} from 'react';
+
+import { Slot, useRouter, useSegments } from 'expo-router';
+
+import "../global.css";
+
+import { AuthContextProvider, useAuth } from '../context/authContext';
+
+import { MenuProviderf } from 'react-native-popup-menu';
+
+
+const MainLayout = () => {
+
+    const { isAuthenticated } = useAuth();
+
+    const segments = useSegments();
+
+    const router = useRouter();
+
+
+    useEffect(() => {
+
+        if (typeof isAuthenticated == 'undefined') return;
+
+        const inApp = segments[0] == '(app)';
+
+        if (isAuthenticated && !inApp) {
+            router.replace('home');
+        } else if(isAuthenticated == false) {
+            router.replace('signIn');
+        }
+    }, [isAuthenticated])
+
+    return <Slot />
+}
+
+export default function RootLayout() {
+    return(
+        <MenuProvider>
+            {/*AuthContextProvider oferece o contexto de autenticação
+            para todo aplicativo*/}
+            <AuthContextProvider>
+                {/* MainLayout gerencia a navegação baseada na autenticação*/}
+                <MainLayout />
+            </AuthContextProvider>      
+        </MenuProvider>
+    )
+}
---
+++ app/index.js
@@ -0,0 +1,22 @@
+import { View, Text, ActivityIndicator } from 'react-native';
+
+import React from 'react';
+
+export default function StartPage() {
+    return(
+
+        <View style={{
+        flex: 1, 
+        justifyContent: 'center'
+        }}>
+            {/* Exibe um indicador de atividade circular, 
+            mostrando ao usuario que algo está carregando*/}
+            
+            <ActivityIndicator 
+            size="large" 
+            color="gray" />
+
+        </View>
+    )
+}
+
