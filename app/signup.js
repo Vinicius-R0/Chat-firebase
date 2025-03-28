8ff32883fc7d11f856e81e7957935ea0d242f1fc
unknown <vinicius_24019@ETEC.FURLAN>
2025-03-28 10:54:40 -0300

    3 commit

--- app/signup.js
+++ app/signup.js
@@ -1,9 +1,26 @@
 import {View, Text, TextInput, Pressable, TouchableOpacity, Pressable, Alert} from 'react-native';
-import React, {useState, useRef} from 'react';
+import React, { useRef} from 'react';
 import { widhtPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 import { StatusBar } from 'expo-status-bar';
 import { Octions } from '@expo/vector-icons';
 import { useRouter } from 'expo-router';
 import Loading from '../components/loading';
 import CustomKeyboardView from '../components/customKeyboard';
-import { useAuth } from '../contexts/auth'; 
+import { useAuth } from '../contexts/authContext';
+
+export default function signup() {
+    const router = useRouter(); //hook do expo-router para navegação
+    const [ loading, setLoading ] = useState(false);
+    const { signup } = useAuth();
+
+    const [formData, setFormData] = useState({
+        email: '',
+        senha: '',
+        username: '',
+        profile: ''
+    });
+
+    const handleChamge = (field, value) => {
+        setFormData(...prev => ({...prev, [field]: value}));
+    };
+}
